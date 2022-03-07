3d434c34d8c9575e77be77d89dbbff88909eafdc
zhangyu <1058769858@qq.com>
2022-03-07 10:03:16 +0800

    add code

---
+++ arithmetic-provider/src/main/java/com/arithmetic/solution/ListNode.java
@@ -0,0 +1,1 @@
+package com.arithmetic.solution;/** * @author Zhang Yu * @date 2022/3/6 10:53 PM */public class ListNode {    int val;    ListNode next;    ListNode(int val) {        this.val = val;    }}
